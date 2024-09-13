7e8a9471c1d4fa3a442520a0ff4b747e46769547
Hyago <159494212+HyagoG59@users.noreply.github.com>
2024-09-13 08:22:14 -0300

    asdf

---
+++ main.js
@@ -0,0 +1,22 @@
+/* Estilos básicos */
+body {
+    font-family: Arial, sans-serif;
+        margin: 0;
+            padding: 0;
+            }
+
+            header {
+                background-color: #f2f2f2;
+                    padding: 20px;
+                    }
+
+                    nav ul {
+                        list-style: none;
+                            padding: 0;
+                                margin: 0;
+                                }
+
+                                nav li {
+                                    display: inline-block;
+                                        margin-right: 10px;
+                                        }
